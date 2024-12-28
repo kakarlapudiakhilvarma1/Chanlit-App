569e92de230f819f3e7b7bf0e1f101cef9b4ad87
Akhil Kakarlapudi <kakarlapudiakhilvarma1@gmail.com>
2024-12-28 11:29:58 +0530

    updated chatapp

---
+++ app.py
@@ -0,0 +1,44 @@
+from langchain_groq import ChatGroq
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+from typing import cast
+
+import chainlit as cl
+
+import os
+
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    model = ChatGroq(model="gemma2-9b-It")
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cast(Runnable, cl.user_session.get("runnable"))  # type: Runnable
+
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
